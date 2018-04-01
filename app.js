d44daa16204fde5120be00d978f7d55ca2d1be14
Lebranc Convas <lebrancconvas@gmail.com>
2018-04-01 14:51:05 +0700

    Add GET Method.

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var express = require('express'); //เรียก express ขึ้นมา
 var app = express();
 var fs = require("fs"); // อ่านไฟล์ user.json
 
+app.get('/getUsers' , function (req, res) {
+  fs.readFile( __dirname + "/" + "user.json" , 'utf8' , function (err, data) {
+    console.log( data ); //ข้อมูลของ User ใน user.json
+    res.end(data); //ส่งข้อมูลมาให้เราดูว่ามีอะไรบ้าง
+  });
+});
+
 var server = app.listen(8081, function(){
   var host = server.address().address
   var port = server.address().port
